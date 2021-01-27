04f0f5810799c6ccc4ab217188cea98522bd3021
alexandra.leschi <alexandra.leschi@hotmail.fr>
2021-01-27 12:00:06 -0500

    updat ehome page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,20 @@ class PagesController < ApplicationController
   def home
      @nav=true
     @document = Document.first
+    @q = Ouvrage.all.ransack(params[:q])
+    @ouvrages = Ouvrage.all
+    @maitre_oeuvre = []
+    @maitre_oeuvre_all = []
+    @array= []
+    @ouvrages.all.each do |ouvrage|
+      if ouvrage.maitre_oeuvre != "SOLIDEO"
+      @maitre_oeuvre << ouvrage.maitre_oeuvre
+      end
+      @maitre_oeuvre_all << ouvrage.maitre_oeuvre
+      @array << ouvrage.name
+    end
+    @maitre_oeuvre.uniq!
+    @maitre_oeuvre_all.uniq!
   end
   
 
