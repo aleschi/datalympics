b08a0581df0c1ff7b6e21751f4ff0e729a434be8
alexandra.leschi <alexandra.leschi@hotmail.fr>
2021-02-02 04:55:31 -0500

    update montant

--- app/models/choru.rb
+++ app/models/choru.rb
@@ -10,7 +10,7 @@ class Choru < ApplicationRecord
       @chorus.operation = row[2]
       @chorus.type_budget = row[3]
       @chorus.centre_financier = row[4]
-      @chorus.compte_budgetaire = row[5]
+      @chorus.compte_budgetaire = row[5]/100
       @chorus.montant = row[6]
       @chorus.domaine_fonctionnel = row[7]
       @chorus.save 
