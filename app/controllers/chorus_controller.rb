4cc01d713980d1aa6792f982f291aab234c40a6a
Alexandra Leschi <alexandra.leschi@hotmail.fr>
2021-08-26 15:04:37 +0200

    update created at params

--- app/controllers/chorus_controller.rb
+++ app/controllers/chorus_controller.rb
@@ -84,7 +84,7 @@ class ChorusController < ApplicationController
 
         @type_pieces=[] 
         if !@programme.nil? && @programme.count > 0
-          @programme = @programme.order('date ASC')
+          @programme = @programme.order('created_at ASC').order('date ASC')
           @programme.each do |choru|
             if choru.type_piece != "RB" && choru.type_piece != "LFI"
             @type_pieces << choru.type_piece
